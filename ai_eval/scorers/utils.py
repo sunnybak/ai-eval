46171d837222f6006785edff33dbac2644063e44
Dhruv Singh <ds3638@columbia.edu>
2024-05-27 19:22:37 -0400

    cost calculation ready

--- ai_eval/scorers/utils.py
+++ ai_eval/scorers/utils.py
@@ -1,4 +1,5 @@
 from openai import OpenAI
+import requests
 
 def openai_call(prompt=None, model='gpt-3.5-turbo', response_format=None):
     if prompt is None:
@@ -17,4 +18,24 @@ def openai_call(prompt=None, model='gpt-3.5-turbo', response_format=None):
     )
     return completion.choices[0].message.content
 
-openai_call('hi')
+def calculate_cost(provider, model, prompt_tokens, completion_tokens):
+    # https://openrouter.ai/api/v1/models
+    url = "https://openrouter.ai/api/v1/models"
+    response = requests.get(url)
+    data = response.json()
+    model_list = data['data']
+
+    found_model = None
+    provider = provider.lower()
+    model_id = provider + "/" + model
+    for model in model_list:
+        if model['id'] == model_id:
+            found_model = model
+            break
+    if found_model is None:
+        raise ValueError(f"Model {model_id} not found in openrouter list")
+
+    pricing = found_model['pricing']
+    cost = completion_tokens * pricing['completion'] + prompt_tokens * pricing['prompt']
+    return cost
+
